a55cdda8cd0561e075247fcbca27215867ed2d9b
ajaypal047 <ajaypal047@gmail.com>
2018-07-31 17:59:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 
 from keras.models import Sequential
 
+from keras import backend as K
+
 #importing Keras structures for standard layers
 from keras.layers import Dropout, Flatten, Dense
 
@@ -18,7 +20,7 @@ from keras.utils.np_utils import to_categorical
 #To plot lost function for observing error function convergence 
 import matplotlib.pyplot as plt
 
-
+import tensorflow as tf
 #openCV for image reading and storing image
 import cv2
 
@@ -38,6 +40,10 @@ img_width, img_height = 224, 224
 # pretrained model weights to use the transfer learning
 top_model_weights_path = 'bottleneck_fc_model.h5'
 
+# from tensorflow.python.client import device_lib
+
+# print("using device       ",device_lib.list_local_devices())
+
 #training data
 train_data_dir = './train_61326'
 
@@ -48,7 +54,7 @@ validation_data_dir = './val_61326'
 epochs = 50
 
 # batch size
-batch_size = 16
+batch_size = 32 
 
 
 #standardization of bool inputs from command line Arguments
@@ -63,6 +69,7 @@ def str2bool(v):
 #function to save output of VGG in .npy files for later use, storing data in .npy saves the calculation and provides freezing of VGG layers
 def saveOutputs():
     # build the VGG16 network
+	
     model = applications.VGG16(include_top=False, weights='imagenet')
 
     #using ImageDataGenerator to scale and manipulate images
@@ -72,7 +79,7 @@ def saveOutputs():
     generator = datagen.flow_from_directory(
         train_data_dir,
         target_size=(img_width, img_height),
-        batch_size=batch_size,
+        batch_size=1,
         class_mode=None,
         shuffle=False)
 
@@ -86,12 +93,15 @@ def saveOutputs():
 
     #saving the output tensor
     np.save('trainOutput.npy', trainOutput)
+		
+    print(np.array(trainOutput).shape)
+    print(np.array(train_samples).shape)
 
     ##### Validation output storing: similar to the train sample ##########
     generator = datagen.flow_from_directory(
         validation_data_dir,
         target_size=(img_width, img_height),
-        batch_size=batch_size,
+        batch_size=1,
         class_mode=None,
         shuffle=False)
 
@@ -107,82 +117,79 @@ def saveOutputs():
 #training the model on top of VGG (i.e. adding the last fully connected layers)
 def train_top_model():
 
-    #defining a ImageDataGenerator from the train directory 
-    datagen_top = ImageDataGenerator(rescale=1. / 255)
+	#defining a ImageDataGenerator from the train directory 
+	datagen_top = ImageDataGenerator(rescale=1. / 255)
 
-    #getting the images from the directory
-    generator_top = datagen_top.flow_from_directory(
-        train_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode='categorical',
-        shuffle=False)
+	#getting the images from the directory
+	generator_top = datagen_top.flow_from_directory(
+		train_data_dir,
+		target_size=(img_width, img_height),
+		batch_size=batch_size,
+		class_mode='categorical',
+		shuffle=False)
 
-    #number of classes 
-    num_classes = len(generator_top.class_indices)
+	#number of classes 
+	num_classes = len(generator_top.class_indices)
 
-    # save the class indices 
-    np.save('class_indices.npy', generator_top.class_indices)
+	# save the class indices 
+	np.save('class_indices.npy', generator_top.class_indices)
 
-    # load the train output for VGG
-    train_data = np.load('trainOutput.npy')
+	# load the train output for VGG
+	train_data = np.load('trainOutput.npy')
+	
+	train_data = train_data
 
-    # get the class lebels for the training data, in the original order
-    train_labels = generator_top.classes
+	# get the class lebels for the training data, in the original order
+	train_labels = generator_top.classes
 
-    train_labels = to_categorical(train_labels, num_classes=num_classes)
+	train_labels = to_categorical(train_labels, num_classes=num_classes)
 
 
 
-    #do the similar steps for the validation set
-    generator_top = datagen_top.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)
 
-    nb_validation_samples = len(generator_top.filenames)
+	#do the similar steps for the validation set
+	generator_top = datagen_top.flow_from_directory(
+		validation_data_dir,
+		target_size=(img_width, img_height),
+		batch_size=batch_size,
+		class_mode=None,
+		shuffle=False)
 
-    validation_data = np.load('validationOutput.npy')
+	nb_validation_samples = len(generator_top.filenames)
 
-    validation_labels = generator_top.classes
-    validation_labels = to_categorical(
-        validation_labels, num_classes=num_classes)
+	validation_data = np.load('validationOutput.npy')
 
-    #building the last fully connected layers
-    model = Sequential()
-    model.add(Flatten(input_shape=train_data.shape[1:]))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='sigmoid'))
+	validation_labels = generator_top.classes
+	validation_labels = to_categorical(validation_labels, num_classes=num_classes)
 
-    #Compiling the model
-    model.compile(optimizer='rmsprop',
-                  loss='categorical_crossentropy', metrics=['accuracy'])
+	#building the last fully connected layers
+	model = Sequential()
+	model.add(Flatten(input_shape=train_data.shape[1:]))
+	model.add(Dense(256, activation='tanh'))
+	model.add(Dropout(0.5))
+	model.add(Dense(num_classes, activation='sigmoid'))
 
-    #saving the log from the training step
-    history = model.fit(train_data, train_labels,
-                        epochs=epochs,
-                        batch_size=batch_size,
-                        validation_data=(validation_data, validation_labels))
+	#Compiling the model
+	model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+	
+	with tf.device('/gpu:0'):
+		history = model.fit(train_data, train_labels, epochs=epochs, batch_size=batch_size, validation_data=(validation_data, validation_labels))
+		print("step Done")
+		# saving the weights
+		model.save_weights(top_model_weights_path)
 
-    #saving the weights
-    model.save_weights(top_model_weights_path)
+		# evaluating on the evaluation set
+		(eval_loss, eval_accuracy) = model.evaluate(validation_data, validation_labels, batch_size=batch_size, verbose=1)
 
-    #evaluating on the evaluation set
-    (eval_loss, eval_accuracy) = model.evaluate(
-        validation_data, validation_labels, batch_size=batch_size, verbose=1)
-
-    plt.figure(1)
-    # plot the accuracy
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
+		plt.figure(1)
+		# plot the accuracy
+		plt.plot(history.history['acc'])
+		plt.plot(history.history['val_acc'])
+		plt.title('model accuracy')
+		plt.ylabel('accuracy')
+		plt.xlabel('epoch')
+		plt.legend(['train', 'test'], loc='upper left')
+		plt.show()
 
 
 def buildModel(image, class_dictionary):
@@ -196,7 +203,7 @@ def buildModel(image, class_dictionary):
         # build additionl fully connected layers
         model = Sequential()
         model.add(Flatten(input_shape=VGGprediction.shape[1:]))
-        model.add(Dense(256, activation='relu'))
+        model.add(Dense(256, activation='tanh'))
         model.add(Dropout(0.5))
         model.add(Dense(num_classes, activation='sigmoid'))
 
@@ -207,34 +214,40 @@ def buildModel(image, class_dictionary):
 def predict(predictDir):
     # load the class_indices saved in the earlier step
 
-    imgFiles = [join(predictDir, f) for f in listdir(predictDir) if isfile(join(predictDir, f))]
+	imgFiles = [join(predictDir, f) for f in listdir(predictDir) if isfile(join(predictDir, f))]
 
-    class_dictionary = np.load('class_indices.npy').item()
+	class_dictionary = np.load('class_indices.npy').item()
 
-    for image_path in imgFiles:
-        
-        orig = cv2.imread(image_path)
+	for image_path in imgFiles:
+		labels = []
+		img = cv2.imread(image_path)
+		img = cv2.resize(img,(3136,3136))
+		for i in (2,3,4,5,6):
+			for j in (0,1):
+				image = img[i*448:(i+1)*448-1, j*448:(j+1)*448-1]
 
-        #print("[INFO] loading and preprocessing image...")
-        image = load_img(image_path, target_size=(224, 224))
-        image = img_to_array(image)
+				#print("[INFO] loading and preprocessing image...")
+				image = cv2.resize(image,(224, 224))
+				image = img_to_array(image)
 
-        # important! otherwise the predictions will be '0'
-        image = image / 255
+				# important! otherwise the predictions will be '0'
+				image = image / 255
 
-        image = np.expand_dims(image, axis=0)
+				image = np.expand_dims(image, axis=0)
 
-        #final class
-        class_predicted= buildModel(image, class_dictionary)
+				#final class
+				class_predicted= buildModel(image, class_dictionary)
 
-        inID = class_predicted[0]
+				inID = class_predicted[0]
 
-        inv_map = {v: k for k, v in class_dictionary.items()}
+				inv_map = {v: k for k, v in class_dictionary.items()}
 
-        label = inv_map[inID]
+				label = inv_map[inID]
+				
+				labels.append(label)
+		
+		print("Image ",image_path, "labels", labels)
 
-        # get the prediction label
-        print("Image ID: {}, Label: {}".format(image_path, label))
 
 def main():
     parser = argparse.ArgumentParser(description="Please Provide Input")
@@ -256,5 +269,5 @@ def main():
         print("ERROR: please provide the testImage directory.")
 
     
-if __name__ == '__main__':
+if _name_ == '_main_':
     main()
